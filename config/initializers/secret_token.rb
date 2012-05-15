0952349664f34c5bddd68764054e15b18d353253
nick hale <nicholas.hale@gmail.com>
2012-05-15 15:30:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootyApp::Application.config.secret_token = '3315a7e2aebc29a676956d30156a71cad3e07c4ef71bccd1767a878068d1ba769e47c6292448457a951dda70cf98579f35a890b60e01bca0c0805f4c14d4da95'
